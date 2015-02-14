8c2930a4ffedde8e8459b1e857fc7d403a88ddb3
Pavel Prokopenko <pavel.a.prokopenko@gmail.com>
2015-02-14 14:44:01 +0200

    update pedestal.service version to 0.3.1

--- project.clj
+++ project.clj
@@ -2,9 +2,9 @@
   :description "a secret door to your Pedestal application"
   :url "http://github.com/propan/geheimtur"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [io.pedestal/pedestal.service "0.3.0"]
+                 [io.pedestal/pedestal.service "0.3.1"]
                  [org.mindrot/jbcrypt "0.3m"]
                  [commons-codec "1.8"]
                  [clj-http "1.0.0"]
