ce9d2e5c603619fb71db54b7e9731cd09ac47a54
Pavel Prokopenko <pavel.a.prokopenko@gmail.com>
2015-05-13 19:43:09 +0300

    update project dependencies to the latest versions

--- project.clj
+++ project.clj
@@ -6,9 +6,9 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [io.pedestal/pedestal.service "0.4.0"]
                  [org.mindrot/jbcrypt "0.3m"]
-                 [commons-codec "1.8"]
-                 [clj-http "1.0.0"]
+                 [commons-codec "1.10"]
+                 [clj-http "1.1.2"]
                  [ring/ring-codec "1.0.0"]
                  ;; integration tests
                  [javax.servlet/javax.servlet-api "3.1.0" :scope "test"]
-                 [org.slf4j/slf4j-nop "1.7.7" :scope "test"]])
+                 [org.slf4j/slf4j-nop "1.7.12" :scope "test"]])
