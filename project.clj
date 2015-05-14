3b8e17475700ddc104183a552dce45543625509b
Pavel Prokopenko <pavel.a.prokopenko@gmail.com>
2015-05-14 16:22:42 +0300

    start 0.1.6-SNAPSHOT version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject geheimtur "0.1.5"
+(defproject geheimtur "0.1.6-SNAPSHOT"
   :description "a secret door to your Pedestal application"
   :url "http://github.com/propan/geheimtur"
   :license {:name "Eclipse Public License"
