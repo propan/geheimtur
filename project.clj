dadb8445d82be19b92251d77037b661985bd7f20
Pavel Prokopenko <pavel.a.prokopenko@gmail.com>
2015-07-25 22:18:40 +0300

    update dependencies

--- project.clj
+++ project.clj
@@ -3,11 +3,14 @@
   :url "http://github.com/propan/geheimtur"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [io.pedestal/pedestal.service "0.4.0"]
                  [org.mindrot/jbcrypt "0.3m"]
                  [commons-codec "1.10"]
-                 [clj-http "1.1.2"]
+                 [clj-http "2.0.0"]
+                 ;; optional dependencies of clj-http
+                 ;; to enable response body coersion
+                 [cheshire "5.4.0"]
                  [ring/ring-codec "1.0.0"]
                  ;; integration tests
                  [javax.servlet/javax.servlet-api "3.1.0" :scope "test"]
