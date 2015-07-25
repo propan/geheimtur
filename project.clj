59e16b7e8268bf9fd59134ccc974bf5a823d64d1
Pavel Prokopenko <pavel.a.prokopenko@gmail.com>
2015-07-25 22:39:25 +0300

    run tests on clojure 1.6 and 1.7

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "http://github.com/propan/geheimtur"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [io.pedestal/pedestal.service "0.4.0"]
+  :exclusions [org.clojure/clojure]
+  :dependencies [[io.pedestal/pedestal.service "0.4.0"]
                  [org.mindrot/jbcrypt "0.3m"]
                  [commons-codec "1.10"]
                  [clj-http "2.0.0"]
@@ -14,4 +14,7 @@
                  [ring/ring-codec "1.0.0"]
                  ;; integration tests
                  [javax.servlet/javax.servlet-api "3.1.0" :scope "test"]
-                 [org.slf4j/slf4j-nop "1.7.12" :scope "test"]])
+                 [org.slf4j/slf4j-nop "1.7.12" :scope "test"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.7.0"]]}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}}
+  :aliases {"all" ["with-profile" "dev,1.6:dev"]})
