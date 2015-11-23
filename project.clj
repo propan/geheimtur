fb375317f948eabfd0af501c3b6bf15fcc1b7bc8
Pavel Prokopenko <pavel.a.prokopenko@gmail.com>
2015-11-23 21:04:17 +0200

    update dependencies

--- project.clj
+++ project.clj
@@ -4,17 +4,17 @@
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :exclusions [org.clojure/clojure]
-  :dependencies [[io.pedestal/pedestal.service "0.4.0"]
+  :dependencies [[io.pedestal/pedestal.service "0.4.1"]
                  [org.mindrot/jbcrypt "0.3m"]
                  [commons-codec "1.10"]
                  [clj-http "2.0.0"]
                  ;; optional dependencies of clj-http
                  ;; to enable response body coersion
-                 [cheshire "5.4.0"]
+                 [cheshire "5.5.0"]
                  [ring/ring-codec "1.0.0"]
                  ;; integration tests
                  [javax.servlet/javax.servlet-api "3.1.0" :scope "test"]
-                 [org.slf4j/slf4j-nop "1.7.12" :scope "test"]]
+                 [org.slf4j/slf4j-nop "1.7.13" :scope "test"]]
   :deploy-repositories [["releases" :clojars {:creds :gpg}]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.7.0"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}}
